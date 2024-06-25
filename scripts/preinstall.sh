957035fe420619e949fc8674b53aadb4f57b68cc
sohankosuru <sohan.kosuru@gmail.com>
2024-06-25 15:23:05 -0400

    network and disk checks

--- scripts/preinstall.sh
+++ scripts/preinstall.sh
@@ -1,9 +1,20 @@
 #! /bin/bash
 echo "Running preinstall checks..."
-UEFIMODE=$(cat /sys/firmware/efi/fw_platform_size)
-if ["$UEFIMODE" -eq 64]; then 
-    echo "UEFI in 64 bit mode"
-else
-    echo "UEFI not in 64 bit mode, exiting"
+ping -c 3 "archlinux.org" > /dev/null 2>&1
+
+# checking for internet
+if ! [ $? -eq 0 ] ; then
+    echo "Error: Internet connection failed. See README for instructions to setup network."
     exit 1
 fi
+
+# checking disk size
+DISKSIZE=$(lsblk -b | awk '$0 ~ /^NAME/ { next } $0 ~ /^([a-z]+|nvme)/ { size=$4 } END { print size }')
+MINREQ=$((8*1024*1024*1024))
+if [ "$DISKSIZE" -lt  "$MINREQ" ]; then
+    echo "Error: Disk size is under minimum 8GB. Current disk size is $DISKSIZE bytes."
+    exit 1
+fi
+
+echo "Passed all preinstall tests, continuing to install"
+exit 0
