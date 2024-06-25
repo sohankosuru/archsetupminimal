d6cb77e23eee216b1e7dc686ebe5050ff28f081e
sohankosuru <sohan.kosuru@gmail.com>
2024-06-25 14:07:24 -0400

    uefi test 2

--- scripts/preinstall.sh
+++ scripts/preinstall.sh
@@ -1,2 +1,9 @@
 #! /bin/bash
-echo "Hello World"
+echo "Running preinstall checks..."
+UEFIMODE=$(cat /sys/firmware/efi/fw_platform_size)
+if ["$UEFIMODE" -eq 64]; then 
+    echo "UEFI in 64 bit mode"
+else
+    echo "UEFI not in 64 bit mode, exiting"
+    exit 1
+fi
